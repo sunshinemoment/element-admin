80fc9630acff5e80cdf2ff935501cc0a734bd66f
biansj <biansj@yunxi.tv>
2021-10-16 09:11:23 +0800

    base form

---
+++ src/views/pro/pro-form/_base-form.js
@@ -0,0 +1,90 @@
+export default {
+  data() {
+    return {
+      baseForm_model: {
+        input1: '',
+        input2: '',
+        input3: '',
+        input4: '',
+        input5: '',
+        input6: '',
+        input7: '',
+        input8: '',
+        input9: '',
+        input10: ''
+      },
+      baseForm_fields: [
+        {
+          type: 'input',
+          formItemProps: {
+            label: '选项一',
+            prop: 'input1'
+          }
+        },
+        {
+          type: 'input',
+          formItemProps: {
+            label: '选项二',
+            prop: 'input2'
+          }
+        },
+        {
+          type: 'input',
+          formItemProps: {
+            label: '选项三',
+            prop: 'input3'
+          }
+        },
+        {
+          type: 'input',
+          formItemProps: {
+            label: '选项四',
+            prop: 'input4'
+          }
+        },
+        {
+          type: 'input',
+          formItemProps: {
+            label: '选项五',
+            prop: 'input5'
+          }
+        },
+        {
+          type: 'input',
+          formItemProps: {
+            label: '选项六',
+            prop: 'input6'
+          }
+        },
+        {
+          type: 'input',
+          formItemProps: {
+            label: '选项七',
+            prop: 'input7'
+          }
+        },
+        {
+          type: 'input',
+          formItemProps: {
+            label: '选项八',
+            prop: 'input8'
+          }
+        },
+        {
+          type: 'input',
+          formItemProps: {
+            label: '选项九',
+            prop: 'input9'
+          }
+        },
+        {
+          type: 'input',
+          formItemProps: {
+            label: '选项十',
+            prop: 'input10'
+          }
+        }
+      ]
+    }
+  }
+}
